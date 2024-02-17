203e04cfa5a89ffae3887b8a0e89c26c9745007e
Kirill <sincerely.manny@gmail.com>
2024-02-17 16:44:43 +0300

    feat: tests

---
+++ src/tree.test.js
@@ -0,0 +1,32 @@
+import { expect, test } from 'vitest';
+import { buildTree, drawTree } from './tree.ts';
+
+test('Test buildind 1-level tree', () => {
+    const input = '(10)';
+    const tree = buildTree(input);
+    expect(tree.tree.length).toBe(1);
+    expect(tree.tree[0].x).toBe(0);
+    expect(tree.tree[0].y).toBe(0);
+    expect(tree.offsets['0']).toBe(2);
+});
+
+test('Test buildind from (1 (2 (4 5 6 (7) 108 (9)) 3))', () => {
+    const input = '(1 (2 (4 5 6 (7) 108 (9)) 3))';
+    const tree = buildTree(input);
+    expect(tree.tree.length).toBe(9);
+});
+
+test('Test drawing from (1 (2 (4 5 6 (7) 108 (9)) 3))', () => {
+    const input = '(1 (2 (4 5 6 (7) 108 (9)) 3))';
+    const { tree, offsets } = buildTree(input);
+    const result = drawTree(tree, offsets);
+    expect(result.trim()).toBe(`1---+
+    2---+
+    |   4
+    |   5
+    |   6-----+
+    |   |     7
+    |   108---+
+    |         9
+    3`);
+});
