e89990dec7a6e6c2eb26322f9f035b17833b72e1
Kirill <sincerely.manny@gmail.com>
2024-02-17 12:49:04 +0300

    feat: fonts set

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
+            },
+        },
     },
     plugins: [],
 } satisfies Config;
