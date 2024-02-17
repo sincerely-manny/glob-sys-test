c051fa287895be144bfd3ce9090944638f7fb584
Kirill <sincerely.manny@gmail.com>
2024-02-17 17:45:30 +0300

    removed comments

--- src/tree.test.js
+++ src/tree.test.js
@@ -20,7 +20,6 @@ test('Test drawing from (1 (2 (4 5 6 (7) 108 (9)) 3))', () => {
     const input = '(1 (2 (4 5 6 (7) 108 (9)) 3))';
     const { tree, offsets } = buildTree(input);
     const result = drawTree(tree, offsets);
-    console.log(offsets);
     expect(result.trim()).toBe(`1---+
     2---+
     |   4
