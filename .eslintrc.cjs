4316259f885aac4cd41e3b39d6798b1378b7f8a7
Kirill <sincerely.manny@gmail.com>
2024-02-17 12:05:07 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-base',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'import'],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020,
+        extraFileExtensions: ['.svelte'],
+    },
+    env: {
+        browser: true,
+        es2017: true,
+        node: true,
+    },
+    rules: {
+        'import/prefer-default-export': ['off'],
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        '@typescript-eslint/indent': ['error', 4],
+    },
+};
